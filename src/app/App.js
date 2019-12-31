9481e0fc0f280be5d14a503a3ae7f7885b532d98
hui-ho <hui-ho@outlook.com>
2019-12-31 11:04:01 +0800

    feat: create Navigation component

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import React from 'react'
+import 'popper.js/dist/popper.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'startbootstrap-clean-blog/css/clean-blog.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AboutBgImg from 'startbootstrap-clean-blog/img/about-bg.jpg'
+import Navigation from '../component/Navigation'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      appName: "DevProj",
+    }
+  }
+
+  render() {
+    let headerStyle = {
+      backgroundImage: `url(${AboutBgImg})`,
+    };
+    return (
+      <div>
+        <Navigation appName={this.state.appName} />
+
+        <header className="masthead" style={headerStyle}>
+          <div className="overlay"></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-md-10 mx-auto">
+                <div className="page-heading">
+                  <h1>About Me</h1>
+                  <span className="subheading">This is what I do.</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/component/Navigation.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+class Navigation extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      menu: [
+        { 'title': '首页', 'path': '/home' },
+        { 'title': '分类', 'path': '/categories' },
+        { 'title': '标签', 'path': '/tags' },
+        { 'title': '留言', 'path': '/contact' },
+        { 'title': '关于', 'path': '/about' },
+      ]
+    }
+  }
+
+  render() {
+    const appName = this.props.appName
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">{appName}</a>
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="fas fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              {
+                this.state.menu.map((item,i) => {
+                  return (
+                    <li className="nav-item" key={i}>
+                      <a className="nav-link" href={item.path}>{item.title}</a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Navigation
