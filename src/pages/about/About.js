a3ccff61204e66963c5dd7bae88f9ba48aac0f7a
hui-ho <hui-ho@outlook.com>
2019-12-31 12:45:38 +0800

    feat: create the about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -8,7 +8,19 @@ class About extends React.Component {
       <div>
         <Header mainHeading="关于" subHeading="刻意练习，持续精进" backgroundImage={BackgroundImage} />
 
-        <h1>About</h1>
+        {/* Main Content */}
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
       </div>
     )
   }
